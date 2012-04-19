aa445243a271787298aca97e71456584c9e86ecd
cethewal@gmail.com <cethewal@gmail.com@6d77d36d-4ea6-f800-6eae-6a383e9ddee5>
2012-04-19 23:10:25 +0000

    from eclipse 20-4-2012   1:09Am before the soundPlayer building

--- alforqan/src/com/waleed/islamic/mainActivity.java
+++ alforqan/src/com/waleed/islamic/mainActivity.java
@@ -5,11 +5,18 @@ package com.waleed.islamic;
  */
 import android.app.Activity;
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SlidingDrawer;
+import android.widget.SlidingDrawer.OnDrawerCloseListener;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,25 +25,38 @@ import android.widget.TextView;
  * @author waleed0
  * main activity
  */
-public class mainActivity extends Activity implements OnClickListener {
+public class mainActivity extends Activity implements OnClickListener, OnDrawerOpenListener, OnDrawerCloseListener {
 	
 	/**
 	 * local fields
 	 */
+	// buttons
+	Button collapseButton;
+	// image buttons
 	ImageButton quranTextPlusZoomButton;
 	ImageButton quranTextMinusZoomButton;
+	// text views
 	TextView quranDisplayTextView;
-	float quranTextSize;
+	// Edit texts
+	EditText ayaRepeatNumEditText;
+	EditText groupRepeatNumEditText;
+	// spinners
 	Spinner suraSelectSpinner;
 	Spinner startAyaSelectSpinner;
 	Spinner endAyaSelectSpinner;
 	Spinner stopPeriodSelectSpinner;
+	// sliding drawers
+	SlidingDrawer settingsSlidingDrawer;
+	// layouts
+	LinearLayout mainPartLayout;
 	//spinner array adapters
 	ArrayAdapter<String> startAyaAdapter, suraNameAdapter, endAyaAdapter, stopPeriodAdapter;
 	//arrays to be used with the adapters
-	String[] suraNamesStrings = {""}, ayaNumStrings = {""}, stopPeriodsStrings = {"طول الاية", "ضعف طول الاية", 
-			"ثلاثة اضعاف طول الاية", "اربعة اضعاف طول الاية"};
-
+	String[] suraNamesStrings = {""}, ayaNumStrings = {""}, stopPeriodsStrings = {"طول الاية", "2 x طول الاية", 
+			"3 x طول الاية", "4 x طول الاية"};
+	// variables
+	float quranTextSize;
+	
 	// Called when activity is first created
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -53,13 +73,24 @@ public class mainActivity extends Activity implements OnClickListener {
 
 	private void initializeGui() {
 		// Get gui references
+		// buttons & image buttons
+		collapseButton = (Button) findViewById(R.id.collapseToggleButton);
 		quranTextPlusZoomButton = (ImageButton) findViewById(R.id.quranTextZoomPlusButton);
 		quranTextMinusZoomButton = (ImageButton) findViewById(R.id.quranTextZoomMinusButton);
+		// Text views
 		quranDisplayTextView = (TextView) findViewById(R.id.quranDisplayTextView);
+		// Spinners
 		suraSelectSpinner = (Spinner) findViewById(R.id.suraSelectSpiner);
 		startAyaSelectSpinner = (Spinner) findViewById(R.id.startAyaSelectSpinner);
 		endAyaSelectSpinner = (Spinner) findViewById(R.id.endAyaSelectSpinner);
 		stopPeriodSelectSpinner = (Spinner) findViewById(R.id.stopPeriodSelectSpinner);
+		// sliding drawers
+		settingsSlidingDrawer = (SlidingDrawer) findViewById(R.id.settingsSlidingDrawer);
+		// layouts
+		mainPartLayout = (LinearLayout) findViewById(R.id.mainBlock);
+		// Edit views
+		ayaRepeatNumEditText = (EditText) findViewById(R.id.ayarepeatenteryEditText);
+		groupRepeatNumEditText = (EditText) findViewById(R.id.grouprepeatenteryEditText);
 		
 		//initialize spinners adapters
 		suraNameAdapter = new ArrayAdapter<String>(this, R.layout.textviewlayout);
@@ -83,8 +114,12 @@ public class mainActivity extends Activity implements OnClickListener {
 		quranTextSize = quranDisplayTextView.getTextSize();
 
 		// set the listeners
+		// buttons listeners
 		quranTextPlusZoomButton.setOnClickListener(this);
-		quranTextMinusZoomButton.setOnClickListener(this);		
+		quranTextMinusZoomButton.setOnClickListener(this);
+		// sliding drawer listeners
+		settingsSlidingDrawer.setOnDrawerOpenListener(this);
+		settingsSlidingDrawer.setOnDrawerCloseListener(this);
 	}
 	
 	void initializeAdapter(ArrayAdapter<String> usedAdapter, String[] usedString){
@@ -95,19 +130,35 @@ public class mainActivity extends Activity implements OnClickListener {
 	}
 
 	/**
-	 * the class controls onclick listener
+	 * the class controls on click listener
 	 * Inputs: v is a reference to the view that caused the event
 	 */
 	@Override
 	public void onClick(View v) {
-		// zooming code
-		if (v == quranTextPlusZoomButton) {
+		switch(v.getId()){
+		// Zoom in
+		case R.id.quranTextZoomPlusButton:
 			quranTextSize++;
 			quranDisplayTextView.setTextSize(quranTextSize);
-		} else if (v == quranTextMinusZoomButton) {
+			break;
+		// zoom out
+		case R.id.quranTextZoomMinusButton:
 			quranTextSize--;
 			quranDisplayTextView.setTextSize(quranTextSize);
+			break;
 		}
+	}
+
+	@Override
+	public void onDrawerClosed() {
+		mainPartLayout.setVisibility(0);  // else show the main part
+		collapseButton.setText(R.string.collapse_button_collapsed); // write suitable text to the button
+	}
+
+	@Override
+	public void onDrawerOpened() {
+		mainPartLayout.setVisibility(4);  // hide the main part
+		collapseButton.setText(R.string.collapse_button_folded); // write suitable text to the button
 	}
 	
 	
